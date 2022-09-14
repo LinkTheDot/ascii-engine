70c23d5179cdceba6588f1c3602b7bc87f611a00
Link <michael.sparks003@gmail.com>
2022-09-14 09:04:35 -0400

    Adds and remakes pixel tests
    In order for tests to be clean and readable, this commit redoes the
    test that checks removing assigned data from a pixel when there's two or
    more objects. It also adds tests for the 'reassign_display_data' function.

--- tests/pixel_tests.rs
+++ tests/pixel_tests.rs
@@ -18,7 +18,7 @@ mod pixel_data_transfer_logic {
   #[test]
   fn move_oldest_object_in_pixel_logic() {
     let mut screen = ScreenData::default();
-    let [object_1_data, object_2_data, _] = generate_all_objects();
+    let [object_1_data, object_2_data, _] = generate_placeholder_objects();
 
     let first_pixel = (0, 0);
     let second_pixel = (1, 0);
@@ -71,7 +71,7 @@ mod remove_displayed_object_logic {
   fn remove_with_one_object() {
     let mut screen = ScreenData::default();
     let pixel_at = (0, 0);
-    let [object_data, _, _] = generate_all_objects();
+    let [object_data, _, _] = generate_placeholder_objects();
     let expected_object_data = Some(object_data.clone());
 
     screen.insert_object_at(&pixel_at, object_data.clone(), true);
@@ -85,48 +85,20 @@ mod remove_displayed_object_logic {
 
   #[test]
   fn remove_with_two_objects() {
-    let mut screen = ScreenData::default();
-    let pixel_at = (0, 0);
-    let [object_data_one, object_data_two, _] = generate_all_objects();
+    let mut pixel_1 = Pixel::new();
+    let [object_1, object_2, _] = generate_placeholder_objects();
 
-    // expected data
-    let expected_removed_object_data = Some(object_data_one.clone());
-    let expected_display_object = OBJECT_2.to_string();
-    let mut expected_display_data = HashMap::new();
-    expected_display_data.insert(0, OBJECT_DISPLAY_2.to_string());
+    let expected_removed_data = Some(object_2.clone());
+    let mut expected_pixel_1_data = Pixel::new();
+    expected_pixel_1_data.insert_object(object_2.0.clone(), object_2.1.clone(), true);
 
-    screen.insert_object_at(&pixel_at, object_data_one.clone(), true);
-    screen.insert_object_at(&pixel_at, object_data_two, true);
+    pixel_1.insert_object(object_1.0, object_1.1, true);
+    pixel_1.insert_object(object_2.0, object_2.1, false);
 
-    screen.change_pixel_display_at(
-      &pixel_at,
-      Some(object_data_one.0),
-      Some(object_data_one.1 .0),
-    );
+    let removed_display_data = pixel_1.remove_displayed_object();
 
-    let removed_displayed_object = screen.remove_displayed_object_data_at(&pixel_at);
-
-    // object should be circle in pixel
-    // removed should be square
-    // assignment should be circle
-
-    // checking if the removed data is correct
-    assert_eq!(removed_displayed_object, expected_removed_object_data);
-
-    // checking if the pixel was correctly updated with the already existing data
-    // key check
-    assert!(screen
-      .get_pixel_at(&pixel_at)
-      .contains_object(&expected_display_object));
-
-    let pixel = screen.get_pixel_at(&pixel_at);
-
-    println!("{:?}", pixel);
-    // object data and key assignment check
-    assert_eq!(
-      pixel.get_all_current_display_data().unwrap(),
-      &expected_display_data
-    );
+    assert_eq!(pixel_1, expected_pixel_1_data);
+    assert_eq!(removed_display_data, expected_removed_data);
   }
 
   #[test]
@@ -141,7 +113,52 @@ mod remove_displayed_object_logic {
   }
 }
 
-fn generate_all_objects() -> [KeyAndObjectDisplay; 3] {
+#[cfg(test)]
+mod reassign_display_data_logic {
+  use super::*;
+
+  #[test]
+  fn available_object() {
+    let mut pixel = Pixel::new();
+    let mut expected_pixel_data = Pixel::new();
+    let [object, _, _] = generate_placeholder_objects();
+
+    expected_pixel_data.insert_object(object.0.clone(), object.1.clone(), true);
+    pixel.insert_object(object.0, object.1, false);
+
+    pixel.reassign_display_data();
+
+    assert_eq!(pixel, expected_pixel_data)
+  }
+
+  #[test]
+  fn multiple_available_objects() {
+    let mut pixel = Pixel::new();
+    let mut expected_pixel_data = Pixel::new();
+    let [object_1, object_2, _] = generate_placeholder_objects();
+
+    expected_pixel_data.insert_object(object_2.0.clone(), object_2.1.clone(), true);
+    expected_pixel_data.insert_object(object_1.0.clone(), object_1.1.clone(), false);
+    pixel.insert_object(object_1.0, object_1.1, false);
+    pixel.insert_object(object_2.0, object_2.1, false);
+
+    pixel.reassign_display_data();
+
+    assert_eq!(pixel, expected_pixel_data)
+  }
+
+  #[test]
+  fn no_available_object() {
+    let mut pixel = Pixel::new();
+    let expected_pixel_data = Pixel::new();
+
+    pixel.reassign_display_data();
+
+    assert_eq!(pixel, expected_pixel_data)
+  }
+}
+
+fn generate_placeholder_objects() -> [KeyAndObjectDisplay; 3] {
   [
     (OBJECT_1.to_string(), (0, OBJECT_DISPLAY_1.to_string())),
     (OBJECT_2.to_string(), (0, OBJECT_DISPLAY_2.to_string())),
